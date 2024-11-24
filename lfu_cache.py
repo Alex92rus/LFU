a418f71e92de816f02babd18d7e89c5aa92b20d6
Alex92rus <aleksandar.rusinov@gmail.com>
2024-11-24 17:41:54 +0000

    This repository contains an implementation of the Least Frequently Used (LFU) cache eviction algorithm with sorting. The LFU algorithm is a caching strategy that prioritizes items based on their frequency of use, ensuring that the least frequently used items are evicted first when the cache reaches its capacity.

---
+++ lfu_cache.py
@@ -0,0 +1,188 @@
+import logging
+
+
+class Dl_node:
+
+    def __init__(self, value):
+        self.prev = None
+        self.value = value
+        self.next = None
+
+
+class DLinkedlist:
+
+    def __init__(self, value):
+        self.head = Dl_node(value)
+        self.tail = self.head
+
+    def add(self, value):
+        self.tail.next = Dl_node(value)
+        self.tail.next.prev = self.tail
+        self.tail = self.tail.next
+
+    def print(self):
+        current = self.head
+        print("None<->", end="")
+        while current:
+            print(f"{current.value}<->", end="")
+            current = current.next
+        print("End Of List", end="")
+
+    def delete_head(self):
+        if self.head is not None:
+            self.head = self.head.next
+            if self.head is not None:
+                self.head.prev = None
+            else:
+                self.tail = None
+        else:
+            print("Empty List")
+
+
+class LFUCache():
+
+    def __init__(self, capacity):
+        self.min_node = None
+        self.capacity = capacity
+        self.value_map = {}
+        self.frequency_map = {}
+        self.key_doubly_list_map = {}
+
+    def read(self, key):
+        if key in self.value_map: # is the key in the cache map
+            try:
+                node: Dl_node = self.frequency_map[key]
+                dictKeys: set = self.key_doubly_list_map[node]
+                dictKeys.remove(key) # remove the key from its frequency sibling keys
+                if len(dictKeys) == 0: # if the key was alone with this frequency, remove the frequency from the list
+                    if node.next is not None and node.next.value == node.value + 1:
+                        next_set: set = self.key_doubly_list_map[node.next]
+                        next_set.add(key)
+                        node.next.prev = node.prev
+                        node.prev.next = node.next
+                        self.frequency_map[key] = node.next
+                    else: # reuse the same node just update its frequency
+                        node.value += 1
+                        dictKeys.add(key)
+                else:
+                    if node.next is not None and node.next.value == node.value + 1:
+                        next_set: set = self.key_doubly_list_map[node.next]
+                        next_set.add(key)
+                        self.frequency_map[key] = node.next
+                    else:
+                        tmp_next = Dl_node(node.value + 1)
+                        tmp_next.next = node.next
+                        tmp_next.prev = node
+                        node.next = tmp_next
+                        self.frequency_map[key] = tmp_next
+                        self.key_doubly_list_map[tmp_next] = {key}
+            except Exception as ex:
+                logging.log(logging.WARN, 'The Initialisation of the LFUis  incorrect')
+                raise Exception('Invalid Setup of LRU')
+            return self.value_map[key]
+        else:
+            return None
+
+    def write(self, key, value):
+        if key not in self.frequency_map:
+            if self.min_node is None:
+                self.min_node = Dl_node(0)
+                self.key_doubly_list_map[self.min_node] = set()
+            if len(self.value_map.keys()) == self.capacity:
+                min_set: set = self.key_doubly_list_map[self.min_node]
+                evict_key = min_set.pop()
+                self.frequency_map.pop(evict_key)
+                self.value_map.pop(evict_key)
+            self.value_map[key] = value
+            if self.min_node.value == 0:
+                min_set: set = self.key_doubly_list_map[self.min_node]
+                min_set.add(key)
+                self.frequency_map[key] = self.min_node
+            else:
+                new_node = Dl_node(0)
+                tmp_node = self.min_node
+                if self.key_doubly_list_map[self.min_node] is None or len(self.key_doubly_list_map[self.min_node]) == 0:
+                    tmp_node = self.min_node.next
+                new_node.next = tmp_node
+                tmp_node.prev = new_node
+                self.min_node = new_node
+                self.key_doubly_list_map[self.min_node] = {key}
+                self.frequency_map[key] = new_node
+        else:
+            self.value_map[key] = value
+
+    def print(self):
+        current = self.min_node
+        print("None<->", end="")
+        while current:
+            print(f"{current.value}<->", end="")
+            current = current.next
+        print("End Of List")
+
+
+
+if __name__ == '__main__':
+    dl_list = DLinkedlist(5)
+    dl_list.add(6)
+    dl_list.add(7)
+    dl_list.print()
+    dl_list.delete_head()
+    print()
+    dl_list.print()
+
+    a_cache: LFUCache = LFUCache(3)
+    a_cache.write(10, 'a')
+    a_cache.write(20, 'b')
+    a_cache.write(30, 'c')
+    a_cache.read(30)
+    a_cache.read(30)
+    a_cache.read(30)
+    a_cache.read(20)
+    a_cache.read(20)
+    a_cache.read(10)
+    a_cache.read(20)
+    a_cache.read(20)
+    a_cache.read(20)
+    a_cache.read(20)
+    a_cache.read(20)
+    a_cache.print()
+    a_cache.write(50, 'c')
+    a_cache.read(50)
+    a_cache.read(30)
+    a_cache.print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,61 @@
+# Efficient LFU Implementation with Sorting of Item Frequencies
+
+## Overview
+This repository contains an implementation of the Least Frequently Used (LFU) cache eviction algorithm with sorting. The LFU algorithm is a caching strategy that prioritizes items based on their frequency of use, ensuring that the least frequently used items are evicted first when the cache reaches its capacity.
+
+## Features
+- **Efficient Cache Management**: Utilizes a sorted data structure - doubly linked list, to keep track of item frequencies and read, evict or add the least frequently used items in O(1).
+- **Configurable Cache Size**: Allows you to set the maximum size of the cache.
+- **Thread Safety**: Ensures thread safety for concurrent access to the cache.
+
+## Implementation
+
+The implementation of the LRU cache is done in Python (3.12), using only built ins - there is no need to install requirements.txt
+packages.
+
+The main class - LFUCache has four methods in total, methods to read and write to the cache, a constructor and a printing function to display
+what resides currently in the cache for debugging purpose.
+
+- **constructor**: creates a LFU cache provided with a capacity.
+- **retrieve method**: attempt to retrieve an item from the cache. This adds a unit (1) frequency to the key which is provided **only** if the key is already in the cache
+- **write method**: writes a key, value pair item into the cache. This method excludes an item from the cache if the cache is full when the addition is called.
+
+### Complexity
+
+The LRU cache uses three data structures
+
+- **value_map: dictionary**: Map which contains the items in the cache. Strings, numbers, and tuples work as keys, and any type can be a value.
+- Other types may or may not work correctly as keys (strings and tuples work cleanly since they are immutable)
+- **frequency_map: dictionary**: Map which contains the frequencies for the keys in the cache
+- **key_doubly_list_map: dictionary**: a *doubly linked list* storing the sorted frequencies connected to a key set map storing the keys with the same frequency
+
+### Sorting
+
+The frequencies are sorted in the doubly linked list which nodes are mapped as keys in key_doubly_list_map: dictionary. Efficient sorting
+can be achieved on the frequencies because at **every addition of a unit to a frequency we have to spend O(1) operations**.
+Therefore, our sorting complexity is split across all the calls of the read method.
+Proving the following:
+At every addition of 1 there can be a maximum of 1 alteration in the frequencies
+1. Empty cache
+starting a new frequency doubly linked list with a single frequency of 1 - O(1)
+2. Addition of 1 to any of the keys having the kth frequency with the value of n
+ - 2.1 (n + 1) is not a frequency - add a new frequency node to the list between th kth and k+1 frequencies - O(1)
+ - 2.2 (n + 1) is a frequency - move the key from the kth frequency to the k+1 frequency set = O(1)
+ - if the key is the only with frequency of n - remove the kth frequency and relink the list - O(1)
+ - 2.3 if the key is the only with frequency of n and (n + 1) is not a frequency increment the frequency of the node in place  - O(1)
+
+Using the above proof we can update the frequency list on every read operation.
+Mind that here we take advantage of two constraints (updating a single number at a time, updating a number with only one value),
+and in total the whole sorting would be O(c), where c is the number of
+retrieve hit calls to the cache, which is equal to the sum of all frequencies in the cache.
+
+When writing a new key, value into the cache, we define a object instance variable min_node to track the head of the list.
+We evict a key from the set mapped to new_node if the capacity has already been reached.
+We create the head of the doubly linked frequency list if it is the first item in the cache, which will have a 0 frequency and 1 key.
+We write the key, value pair in the cache value map, if the key is already there, the value is overridden to the new from te call.
+
+## Installation
+1. Clone this repository:
+   ```sh
+   git clone https://github.com/Alex92rus/LFU
+   python lfu_cache.py
